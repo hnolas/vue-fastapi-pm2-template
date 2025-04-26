0b2a4d9702d4c6bdac6f52876654562c1b61024a
hnolas19 <hnolas19@asu.edu>
2025-04-26 13:33:28 -0700

    fix

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-load_dotenv(".env")
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 import secrets
 from typing import Any, Dict, List, Optional, Union
 
